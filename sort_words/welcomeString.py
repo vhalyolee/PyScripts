7078e1913c0015456348038dab69a8d08c1d5e00
vhalyolee <vhalyolee@users.noreply.github.com>
2017-06-26 12:47:03 -0400

    welcome message

---
+++ sort_words/welcomeString.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+first_name= "Valerie"
+last_name = "Halyo"
+
+print "Hello " + first_name+ " " + last_name
+
+number_char = len("Hello ") + len(fst_name) + 1 + len(last_name)
+print "=" * number_char
